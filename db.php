82a6eea0c828f85a343c1158c749f078b7784405
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-12-01 20:39:48 +0200

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 
 require "config/constants.php";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "exam";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b418eaaffbbae2";
+$password = "261f9bfb";
+$db = "heroku_8ad4d708550d9cd";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
